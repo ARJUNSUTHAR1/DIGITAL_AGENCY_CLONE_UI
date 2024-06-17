d5d55c7caefe5a0e9839548cf7eb17edaa995fde
Arjun <arjunsuthar2112@gmail.com>
2024-06-17 10:48:29 +0530

    landing page and nav added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Nav from './components/navbar/Nav'
+import LandingPage from './pages/LandingPage'
+function App() {
+
+  return (
+    <>
+     <div className='main'>
+      <Nav/>
+      <LandingPage/>
+     </div>
+    </>
+  )
+}
+
+export default App
